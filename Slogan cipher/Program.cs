4f807391f85d23e7534beb318b25942cebfaaf97
Sergey <vashenko49@gmail.com>
2019-09-04 01:23:52 +0300

    finished Polybian Square Slogan cipher

--- Slogan cipher/Program.cs
+++ Slogan cipher/Program.cs
@@ -11,6 +11,29 @@ namespace Slogan_cipher
         static void Main(string[] args)
         {
             Console.WriteLine("Slogan cipher");
+            Console.WriteLine("Enter phrase for cipher");
+            var phrase = Console.ReadLine();
+
+            Console.WriteLine("Enter key word");
+            var key = Console.ReadLine();
+
+            const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            var tableTemplate = (key + alphabet).ToCharArray().Distinct().ToArray();
+
+            var result = "";
+
+            foreach (var letter in phrase)
+            {
+                for (int i = 0; i < alphabet.Length; i++)
+                {
+                    if (Char.ToUpper(letter) == Char.ToUpper(alphabet[i]))
+                    {
+                        result += Char.IsLower(letter) ? tableTemplate[i] : Char.ToUpper(tableTemplate[i]);
+                    }
+                }
+            }
+
+            Console.WriteLine(result);
             Console.ReadKey();
         }
     }
