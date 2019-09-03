a8a8f0a8678b66137e88fa3c5ebf9d3b2fd159f4
Sergey <vashenko49@gmail.com>
2019-09-04 01:25:42 +0300

    create

---
+++ Polybian Square/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polybian_Square
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            Console.WriteLine("Enter phrase");
+            string phras = Console.ReadLine();
+
+            if (phras != null)
+            {
+                int h = Convert.ToInt32(Math.Ceiling(Math.Sqrt(alphabet.Length)));
+                var quard = new Char[h,h];
+
+                //заполнили таблицу
+                for (int x = 0, letter =0; x < h; x++)
+                {
+                    for (int y = 0; y < h; y++)
+                    {
+                        quard[x, y] = letter < alphabet.Length ? (alphabet[letter++]) : '-';
+                    }
+                }
+
+                string result = "";
+
+                //шифруем
+                foreach (var letter in phras)
+                {
+                    for (int x = 0; x < h; x++)
+                    {
+                        for (int y = 0; y < h; y++)
+                        {
+                            if (letter == quard[x, y])
+                            {
+                                result+=Convert.ToString(x)+":"+Convert.ToString(y) + " ";
+                            }
+                        }
+                    }
+                }
+
+                Console.WriteLine(result);
+                Console.ReadKey();
+            }
+
+
+        }
+    }
+}
