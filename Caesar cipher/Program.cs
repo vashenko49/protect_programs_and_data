b89034167a9af3cae962d2092f3bbf9c6be4b940
Sergey <vashenko49@gmail.com>
2019-09-03 15:32:53 +0300

    fixed proj

---
+++ Caesar cipher/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caesar_cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Цезарь");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Slogan cipher/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slogan_cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Slogan cipher");
+            Console.ReadKey();
+        }
+    }
+}
