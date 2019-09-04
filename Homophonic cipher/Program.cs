39e4c5ae050946786ba5cdb8f34d2d1e2db3d99d
Sergey <vashenko49@gmail.com>
2019-09-04 03:46:25 +0300

    finished pw1

---
+++ Homophonic cipher/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homophonic_cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Homophonic cipher");
+
+            Console.WriteLine("Enter word:");
+            var s1 = Console.ReadLine();
+            var s2 = string.Empty;
+            if (s1 != null)
+                foreach (var t in s1)
+                {
+                    if (t != ' ')
+                        if (t == 1071)
+                            s2 += (char)(1103);
+                        else if (t == 1040)
+                            s2 += (char)(1071);
+                        else
+                            s2 += (char)(t - 1);
+                    else
+                        s2 += t;
+                }
+
+            Console.WriteLine("Replaced word {0}", s2);
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Vizhener's cipher/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vizhener_s_cipher
+{
+
+    class Program
+    {
+        private static readonly char[] Characters = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+            'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+            'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+        private static readonly int N = Characters.Length;
+
+        private static string Encode(string input, string keyword)
+        {
+            input = input.ToUpper();
+            keyword = keyword.ToUpper();
+
+            var result = "";
+            var keywordIndex = 0;
+
+            foreach (var symbol in input)
+            {
+                var c = (Array.IndexOf(Characters, symbol) +
+                         Array.IndexOf(Characters, keyword[keywordIndex])) % N;
+
+                result += Characters[c];
+
+                keywordIndex++;
+
+                if ((keywordIndex + 1) == keyword.Length)
+                    keywordIndex = 0;
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vizhener's cipher");
+
+            Console.WriteLine("Enter phrase for cipher");
+            var inputEncode = Console.ReadLine();
+
+            Console.WriteLine("Enter key word");
+            var key = Console.ReadLine();
+
+
+            Console.WriteLine(Encode(inputEncode, key));
+            Console.ReadKey(true);
+
+        }
+    }
+}
