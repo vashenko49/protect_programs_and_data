ef850b0e10fbc07ed96bf60bdc12cdef7d6def01
Sergey <vashenko49@gmail.com>
2019-09-09 01:00:39 +0300

    finished simp and blovk

---
+++ block single permutation/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace block_single_permutation
+{
+    class Program
+    {
+        static Random random = new Random((int)DateTime.Now.Ticks);
+        static string RandomString()
+        {
+            StringBuilder builder = new StringBuilder();
+            char ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+            builder.Append(ch);
+            return builder.ToString();
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("block single permutation");
+            Console.WriteLine("Enter phrase");
+            string phrase = Console.ReadLine();
+            Console.WriteLine("Enter key");
+            string key = Console.ReadLine();
+
+            int[,] table = new int[2,key.Length];
+
+            for (int i = 0; i < key.Length; i++)
+            {
+                table[0, i] = i;
+            }
+
+            for (int i = 0; i < key.Length; i++)
+            {
+                Console.WriteLine(key[i]);
+                table[1, i] = Convert.ToInt32(Convert.ToString(key[i]));
+            }
+
+            string result = "";
+
+            for (int i = 0, len = 0; i < Math.Ceiling(Convert.ToDouble(phrase.Length) / Convert.ToDouble(key.Length)); i++, len+=key.Length)
+            {
+                for (int j = 0; j <key.Length; j++)
+                {
+                    int temp = table[0, j];
+
+                    for (int k = 0; k < key.Length; k++)
+                    {
+                        if (temp == table[1, k])
+                        {
+                            temp = table[0, k];
+                            break;
+                        }
+
+                    }
+
+                    result += temp + len >= phrase.Length ? Convert.ToChar(RandomString()) : phrase[temp + len];
+                }
+            }
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
--- simple single permutation/Program.cs
+++ simple single permutation/Program.cs
@@ -10,6 +10,50 @@ namespace simple_single_permutation
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("simple single permutation");
+            Console.WriteLine("Enter phrase");
+            string phrase = Console.ReadLine();
+
+            int[,] table = new int[2,phrase.Length];
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < phrase.Length; j++)
+                {
+                    table[i, j] = j;
+                }
+            }
+            Random rand = new Random();
+
+            for (int i = phrase.Length - 1; i >= 1; i--)
+            {
+                int j = rand.Next(i + 1);
+
+                int tmp = table[1,j];
+                table[1,j] = table[1,i];
+                table[1,i] = tmp;
+            }
+
+            string result = "";
+
+            for (int i = 0; i < phrase.Length; i++)
+            {
+                int temp = table[0, i];
+
+                for (int j = 0; j < phrase.Length; j++)
+                {
+                    if (temp == table[1, j])
+                    {
+                        temp = table[0, j];
+                        break;
+                    }
+                }
+
+                result += phrase[temp];
+            }
+
+            Console.WriteLine("encrypted phrase");
+            Console.WriteLine(result);
+            Console.ReadKey();
         }
     }
 }
