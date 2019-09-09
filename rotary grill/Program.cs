caef49bf7a8dfd6c7034eb25ed483a7d16d19b70
Sergey <vashenko49@gmail.com>
2019-09-09 22:11:54 +0300

    finished magic square

--- rotary grill/Program.cs
+++ rotary grill/Program.cs
@@ -8,8 +8,32 @@ namespace rotary_grill
 {
     class Program
     {
+        static int[] randomCoord(int max, int min)
+        {
+            int[] result = new int[2];
+            
+            result[0] = new Random().Next(min, max);
+            result[1] = new Random().Next(min, max);
+
+            return result;
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("rotary grill");
+            Console.WriteLine("Enter phrase");
+            string phrase = Console.ReadLine();
+
+            int h = Math.Ceiling(Math.Sqrt(phrase.Length)) % 2 == 0 ? Convert.ToInt32(Math.Ceiling(Math.Sqrt(phrase.Length))) : Convert.ToInt32(Math.Ceiling(Math.Sqrt(phrase.Length))) + 1;
+
+            string[,] template = new string[h,h];
+            string[,] table = new string[h,h];
+
+
+
+
+
+            Console.WriteLine(h);
+            Console.ReadKey();
         }
     }
 }
