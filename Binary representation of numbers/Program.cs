fab0d9d2d3d105ccb356cf905271f86ed7f62769
Sergey <vashenko49@gmail.com>
2019-09-16 17:18:38 +0300

    finish pw6

--- Binary representation of numbers/Program.cs
+++ Binary representation of numbers/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Binary_representation_of_numbers
 {
@@ -15,12 +11,15 @@ namespace Binary_representation_of_numbers
             {
                 throw new System.InvalidOperationException("Arrays must be equal in length");
             }
+
             int[] result = new int[op1.Length];
             for (int i = 1; i < op1.Length + 1; i++)
             {
-                if (op1[op1.Length - i] > 1 || op2[op2.Length - i] > 1 || op1[op1.Length - i] < 0 || op2[op2.Length - i] < 0)
+                if (op1[op1.Length - i] > 1 || op2[op2.Length - i] > 1 || op1[op1.Length - i] < 0 ||
+                    op2[op2.Length - i] < 0)
                 {
-                    throw new System.ArgumentOutOfRangeException("Array has a number that is invalid in it, argument out of range.");
+                    throw new System.ArgumentOutOfRangeException(
+                        "Array has a number that is invalid in it, argument out of range.");
                 }
                 else
                 {
@@ -35,13 +34,17 @@ namespace Binary_representation_of_numbers
                             int[] escape = new int[1];
                             return escape;
                         }
+
                         try
                         {
                             result[result.Length - i] = 0;
                             result[result.Length - i - 1] = 1;
                         }
-                        catch { }
+                        catch
+                        {
+                        }
                     }
+
                     if (result[result.Length - i] == 3)
                     {
                         if (result.Length - i - 1 < 0)
@@ -59,14 +62,92 @@ namespace Binary_representation_of_numbers
                                 result[result.Length - i] = 1;
                                 result[result.Length - i - 1] = 1;
                             }
-                            catch { }
+                            catch
+                            {
+                            }
                         }
                     }
                 }
             }
+
             return result;
         }
-    
+
+        static string SignedMagnitudeRepresentation(double input)
+        {
+            string result = "";
+            if (input < 0)
+            {
+                input = Math.Abs(input);
+                result += "1";
+            }
+            else
+            {
+                result += "0";
+            }
+            result += IntBitConvert(input, false, true);
+            return result;
+        }
+        static string  OnesComplement(double input)
+        {
+            string res = SignedMagnitudeRepresentation(input);
+
+            for (int i = 1; i < res.Length; i++)
+            {
+                res = res[i] == '0' ? res.Insert(i, "1") : res.Insert(i, "0");
+                res = res.Remove(i+1, 1);
+
+            }
+
+            return res;
+        }
+
+        static string TwosComplement(double input)
+        {
+            string res = OnesComplement(input);
+
+            int[] ones = new int[res.Length-1];
+            int[] addition = new int[res.Length-1];
+            addition[addition.Length - 1] = 1;
+            for (int i = 1; i < res.Length; i++)
+            {
+                ones[i - 1] = Convert.ToInt32(Convert.ToString(res[i]));
+            }
+
+            int[] result = AddBinary(ones, addition);
+
+            res = res[0] + string.Join("", result);
+
+            return res;
+        }
+
+        static string FloatingPoint(double input)
+        {
+            string result = "";
+            if (input < 0)
+            {
+                input = Math.Abs(input);
+                result += "1";
+            }
+            else
+            {
+                result += "0";
+            }
+
+            result += IntBitConvert(input, true,false);
+            Console.WriteLine(result);
+
+            int indexComma = result.IndexOf('.');
+            if (indexComma > 0)
+            {
+                Console.WriteLine(indexComma);
+                result = result.Remove(indexComma,1);
+                result = result.Insert(2, ".");
+            }
+            result += $"*2^{(indexComma > 2 ? indexComma - 2 : 0)}";
+
+            return result;
+        }
 
         static int exp = 0;
         static int dotPos = 0;
@@ -81,7 +162,7 @@ namespace Binary_representation_of_numbers
                 sign = 0;
 
             input = Math.Abs(input);
-            string bits = IntBitConvert(input);
+            string bits = IntBitConvert(input, false, false);
             string normalized = "";
             if (bits.Length > 1)
                 normalized = bits.Insert(1, ".");
@@ -96,7 +177,7 @@ namespace Binary_representation_of_numbers
                 expBits = "01111111";
             else
             {
-                expBits = IntBitConvert(newExp);
+                expBits = IntBitConvert(newExp, false, false);
                 if (expBits.Length > 8)
                     expBits = expBits.Substring(0, 8);
                 else
@@ -116,7 +197,7 @@ namespace Binary_representation_of_numbers
 
         }
 
-        static String IntBitConvert(double input)
+        static String IntBitConvert(double input, bool comma, bool fractionalPart)
         {
             double floor = Math.Floor(input);
             double frac = input - floor;
@@ -145,12 +226,15 @@ namespace Binary_representation_of_numbers
                 exp = bits.Length - 1;
             }
 
-            if (frac == 0)
+            if (frac == 0 || fractionalPart)
                 return bits;
             dotPos = bits.Length;
             temp = 0;
             e = -1;
-            bits += ".";
+            if (comma)
+            {
+                bits += ".";
+            }
             while (temp <= frac && e > -80)
             {
                 if (temp + Math.Pow(2, e) <= frac)
@@ -184,30 +268,37 @@ namespace Binary_representation_of_numbers
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter");
+            Console.WriteLine("Введите число для перевода в в прямой код:");
             string inp = Console.ReadLine();
             double result = 0;
-            if (!Double.TryParse(inp, out result))
-                Console.WriteLine("Invalid input.");
-            else
-            {
-                string f32 = FloatConvert32(result);
-                Console.WriteLine(f32);
-            }
+            Console.WriteLine(!Double.TryParse(inp, out result)
+                ? "Invalid input."
+                : SignedMagnitudeRepresentation(result));
 
-            double dfg = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число для перевода в обратный код:");
+            inp = Console.ReadLine();
+            Console.WriteLine(!Double.TryParse(inp, out result)
+                ? "Invalid input."
+                : OnesComplement(result));
 
-            Console.WriteLine(IntBitConvert(dfg));
+            Console.WriteLine("Введите число для перевода в дополнительный код:");
+            inp = Console.ReadLine();
+            Console.WriteLine(!Double.TryParse(inp, out result)
+                ? "Invalid input."
+                : TwosComplement(result));
 
-            int[] one = new[] {0, 0, 0, 1, 1};
-            int[] two = new int[one.Length];
-            two[one.Length - 1] = 1;
-            int[] temp = AddBinary(one, two);
+            Console.WriteLine("Введите число для перевода двоичный в код в формате с плавающей запятой, включающего знаки порядка и мантиссы: ");
+            inp = Console.ReadLine();
+            Console.WriteLine(!Double.TryParse(inp, out result)
+                ? "Invalid input."
+                : FloatingPoint(result));
 
+            Console.WriteLine("Введите число для перевода в формат одинарной точности по стандарту IEEE 754: ");
+            inp = Console.ReadLine();
+            Console.WriteLine(!Double.TryParse(inp, out result)
+                ? "Invalid input."
+                : FloatConvert32(result));
 
-            Console.WriteLine(string.Join("",one));
-            Console.WriteLine(string.Join("",two));
-            Console.WriteLine(string.Join("",temp));
             Console.ReadKey();
 
         }
