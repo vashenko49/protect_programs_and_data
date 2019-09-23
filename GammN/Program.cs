4654f00fbc2d52018f0ef1d5203b2d79017dae65
Sergey <vashenko49@gmail.com>
2019-09-23 23:18:33 +0300

    create gamma module 2

--- GammN/Program.cs
+++ GammN/Program.cs
@@ -1,88 +1,59 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.NetworkInformation;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GammN
 {
-    class Program
+    internal class Program
     {
-        private static string alphabet = "abcdefghijklmnopqrstuvwxyz";
-        static void Main(string[] args)
+        private static readonly string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        private static void Main(string[] args)
         {
             Console.WriteLine("Gamma modulo N");
             Console.WriteLine("Enter phrase:");
-            string phrase = Console.ReadLine();
+            var phrase = Console.ReadLine();
             Console.WriteLine("Enter gamma(key string):");
-            string key = Console.ReadLine();
+            var key = Console.ReadLine();
 
-            int N = alphabet.Length;
+            var N = alphabet.Length;
 
             if (phrase.Length > key.Length)
-            {
-                for (int j=0, i = key.Length; i < phrase.Length; i++,j++)
-                {
+                for (int j = 0, i = key.Length; i < phrase.Length; i++, j++)
                     key += key[j];
-                }
-            }else if (key.Length>phrase.Length)
-            {
-                for (int i = phrase.Length; i < key.Length; i++)
-                {
+            else if (key.Length > phrase.Length)
+                for (var i = phrase.Length; i < key.Length; i++)
                     phrase += Convert.ToString(alphabet[alphabet
                                                             .Length - 1]);
 
-                }
-            }
+            var table = new int[5, phrase.Length];
 
-            int[,] table = new int[5,phrase.Length];
-
-            for (int i = 0; i <phrase.Length; i++)
+            for (var i = 0; i < phrase.Length; i++)
+            for (var j = 0; j < alphabet.Length; j++)
             {
-                for (int j = 0; j <alphabet.Length; j++)
-                {
-                    if (Convert.ToString(key[i]).ToLower() == Convert.ToString(alphabet[j]).ToLower())
-                    {
-                        table[1, i] = j;
-                    }
-                    if (Convert.ToString(phrase[i]).ToLower() == Convert.ToString(alphabet[j]).ToLower())
-                    {
-                        table[0, i] = j;
-                    }
-                }
+                if (Convert.ToString(key[i]).ToLower() == Convert.ToString(alphabet[j]).ToLower()) table[1, i] = j;
+                if (Convert.ToString(phrase[i]).ToLower() == Convert.ToString(alphabet[j]).ToLower()) table[0, i] = j;
             }
-
 
 
             //По модулю N. N берем как длина алфавита 
-            for (int i = 0; i < key.Length; i++)
-            {
-                table[2, i] = (table[0, i] + table[1, i])%N;
-            }
+            for (var i = 0; i < key.Length; i++) table[2, i] = (table[0, i] + table[1, i]) % N;
 
-            for (int i = 0; i < key.Length; i++)
+            for (var i = 0; i < key.Length; i++)
             {
-                table[3, i] = (table[2, i] + N - table[1, i]);
+                table[3, i] = table[2, i] + N - table[1, i];
                 table[4, i] = table[3, i] % N;
             }
 
 
-            for (int i = 0; i < 5; i++)
+            for (var i = 0; i < 5; i++)
             {
-                for (int j = 0; j < phrase.Length; j++)
-                {
+                for (var j = 0; j < phrase.Length; j++)
                     if (i != 3)
-                    {
-                        Console.Write(alphabet[table[i,j]] + " " + table[i,j] + " ");
-                    }
+                        Console.Write(alphabet[table[i, j]] + " " + table[i, j] + " ");
                     else
-                    {
-                        Console.Write("  "+table[i,j] + " ");
-                    }
-                }
+                        Console.Write("  " + table[i, j] + " ");
                 Console.WriteLine();
             }
+
             Console.ReadLine();
         }
     }
