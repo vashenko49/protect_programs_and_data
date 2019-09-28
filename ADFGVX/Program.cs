af9261ae95f0e60cd4dca68d068f5aacb03b2e20
Sergey <vashenko49@gmail.com>
2019-09-28 13:51:33 +0300

    preparation

---
+++ ADFGVX/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADFGVX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ADFGVX");
+            Console.WriteLine("Enter phrase:");
+            string phrase = Console.ReadLine();
+        }
+    }
+}
