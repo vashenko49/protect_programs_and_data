ac908b8c34adc9f65e3cfa27cbccdde5e3f2f43a
Sergey <vashenko49@gmail.com>
2019-09-30 18:31:35 +0300

    add adfgvx

--- ADFGVX/Program.cs
+++ ADFGVX/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,11 +9,120 @@ namespace ADFGVX
 {
     class Program
     {
+        private static readonly string alphabet = "abcdefghijklmnopqrstuvwxyz ,-+_0123456789.";
+
         static void Main(string[] args)
         {
             Console.WriteLine("ADFGVX");
             Console.WriteLine("Enter phrase:");
             string phrase = Console.ReadLine();
+
+            char[,] table = new char[7,7];
+
+
+
+            for (int i = 1; i < table.GetLength(0); i++)
+            {
+                table[0, i] = alphabet[i-1];
+                table[i, 0] = alphabet[i-1];
+            }
+
+            for (int i = 1,g=alphabet.Length-1; i < table.GetLength(0); i++)
+            {
+                for (int j = 1; j < table.GetLength(1); j++, g--)
+                {
+                    table[i, j] = g >= 0 ? alphabet[g] : '-';
+                }
+            }
+
+            List<char> preResult = new List<char>();
+
+            foreach (var element in phrase)
+            {
+                for (int i = 1; i < table.GetLength(0); i++)
+                {
+                    for (int j = 1; j < table.GetLength(1); j++)
+                    {
+                        if (table[i, j] == element)
+                        {
+                            preResult.Add(table[i, 0]);
+                            preResult.Add(table[0, j]);
+                        }
+                    }
+                }
+            }
+
+            char[,] preTable = new char[phrase.Length, 2+Convert.ToInt32(Math.Ceiling(Convert.ToDouble(preResult.Count) / Convert.ToDouble(phrase.Length)))];
+
+
+            for (int i = 0; i < phrase.Length; i++)
+            {
+                preTable[i, 0] = phrase[i];
+
+            }
+
+            for (int i = 0,b=0; i < alphabet.Length; i++)
+            {
+                for (int j = 0; j< preTable.GetLength(0); j++)
+                {
+                    if (alphabet[i] == Convert.ToChar(preTable[j, 0].ToString().ToLower()))
+                    {
+                        preTable[j, 1] = Convert.ToChar(b++);
+                    }
+                }
+            }
+
+            for (int i = 2, g =0; i < preTable.GetLength(1); i++)
+            {
+                for (int j = 0; j < preTable.GetLength(0); j++, g++)
+                {
+                    preTable[j, i] = g < preResult.Count ? preResult[g] : '-';
+                }
+            }
+
+
+            for (int i = 0; i < preTable.GetLength(1); i++)
+            {
+                for (int j = 0; j < preTable.GetLength(0); j++)
+                {
+                    if (i == 1)
+                    {
+                        Console.Write(Convert.ToInt32(preTable[j, i]));
+
+                    }
+                    else
+                    {
+                        Console.Write(Char.ToString(preTable[j, i]));
+
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+
+            string result = "";
+
+            int temp = 0;
+            while (temp<phrase.Length)
+            {
+                for (int i = 0; i < phrase.Length; i++)
+                {
+                    if (temp == Convert.ToInt32(preTable[i, 1]))
+                    {
+                        temp++;
+                        for (int j = 2; j < preTable.GetLength(1); j++)
+                        {
+                            result += preTable[i, j];
+                        }
+
+                        result += " ";
+                    }
+                }
+            }
+
+            Console.WriteLine(result);
+            Console.ReadLine();
         }
     }
 }
